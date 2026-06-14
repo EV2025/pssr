1be646c975f1ecb3df0e8cf409ed3acc1da0ee34
EV2025 <equilibrevital.bruxelles@gmail.com>
2026-06-14 21:11:53 +0200

    pssr6

--- wp-content/themes/hostinger-ai-theme/gutenberg-blocks/Booking/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '160224873b0dcba1c6bc');
--- wp-content/themes/hostinger-ai-theme/gutenberg-blocks/Booking/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'f5ed129f4cc55bc32710');
