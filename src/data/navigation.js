3984818c80856b285efdc0a0eef49b5d8b97007b
EV2025 <equilibrevital.bruxelles@gmail.com>
2026-06-21 18:25:08 +0200

    pssr 46

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -11,16 +11,12 @@ export const actionItems = [
 
 export const footerNavigation = [
   { label: 'Accueil', href: '/pssr/' },
-  { label: 'Activités', href: '/pssr/activites.html' },
-  { label: 'Dispositifs', href: '/pssr/dispositifs.html' },
-  { label: 'PSSR', href: '/pssr/pssr.html' },
-  { label: 'Ateliers', href: '/pssr/dispositifs.html#ateliers' },
-  { label: 'Ressources', href: '/pssr/documents.html' },
+  { label: 'Espace coach', href: '/pssr/coach/' },
+  { label: 'Documents', href: '/pssr/documents.html' },
+  { label: 'Action externe', href: '/pssr/partenaires-institutions.html' },
 ];
 
 export const footerPartners = [
-  { label: 'Partenaires & institutions', href: '/pssr/partenaires-institutions.html' },
-  { label: 'Actions partenaires', href: '/pssr/partenaires-institutions.html#actions-partenaires' },
-  { label: 'Écoles & ATL', href: '/pssr/ecoles-atl.html' },
+  { label: 'ATL', href: '/pssr/ecoles-atl.html' },
   { label: 'Ressources emploi & insertion', href: '/pssr/ressources-emploi-insertion.html' },
 ];
